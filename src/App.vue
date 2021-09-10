7e34e0bc4fe446cd1400c7ead1f0f8e19f524f6a
ElSamirito <84684637+ElSamirito@users.noreply.github.com>
2021-09-10 01:09:15 -0300

    Ejercicio 2, al fin logro que sume

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   <div id="app">
     <h1>Ejercicio N°1</h1>
     <Ejercicio1 />
+    <h1>Ejercicio N°2</h1>
+    <Ejercicio2 />
   </div>
 </template>
 
 <script>
 import Ejercicio1 from "./components/Ejercicio1.vue"
+import Ejercicio2 from "./components/Ejercicio2.vue"
 
 export default {
   components: {
     Ejercicio1,
+    Ejercicio2,
   },
   name: 'App',
 }
---
+++ src/components/Ejercicio2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input v-model.number="num1" type="text">
+    <input v-model.number="num2" type="text">
+    <p>{{resultado(num1, num2)}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ejercicio2',
+  data: function(){
+    
+    return {
+      // put your variables here...
+      num1: 1,
+      num2: 1,
+    }
+  },
+  methods: {
+    resultado(a, b){
+      return a+b
+    }
+    // put your methods here...
+  }
+}
+</script>
+
+<style>
+
+</style>
